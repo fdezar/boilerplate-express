6045689c37ff90de1a7cf2f05e4eddfa5eb27e74
Gabriel Fernandez <gfernandezareas@gmail.com>
2023-07-13 13:34:28 +0200

    new exercises

--- myApp.js
+++ myApp.js
@@ -1,6 +1,33 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+/* app.get("/json", (req, res) => {
+    res.json(  
+        { "message": "Hello json" }
+        );
+});
+
+*/
+
+app.get("/json", (req, res) => {
+    let jsonResponse = { "message": "Hello json"};
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        jsonResponse.message = jsonResponse.message.toUpperCase();
+    }
+
+    res.json(jsonResponse);
+});
+
 console.log("Hello World");
 
 
